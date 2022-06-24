882096ad76121319bec41a1440085c42ae26a979
sudoHungNN <>
2022-06-24 04:23:36 +0000

    Use Firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBvPilClD3gmfr7QQabqLsDzLF6PQfdY84",
+  authDomain: "itss-firebase-b96c0.firebaseapp.com",
+  projectId: "itss-firebase-b96c0",
+  storageBucket: "itss-firebase-b96c0.appspot.com",
+  messagingSenderId: "673986125076",
+  appId: "1:673986125076:web:faed37e9430271524a7601",
+  measurementId: "G-MGPJR2F6ZS"
+};
+
+firebase.initializeApp(firebaseConfig);
